729a3c53134f23c59336344cf862b55ebf72dff1
nahelsamson <nahelsamson@free.fr>
2024-10-15 10:48:18 +0200

    Add files via upload

---
+++ code.js
@@ -0,0 +1,45 @@
+function AfficheDate(){	
+	document.getElementById('date').innerHTML=Date();
+}
+function ModifTexteRouge(){
+	element = document.getElementById('TexteModif');
+	element.style.color='red'
+}
+function ModifTexteVert(){
+	element = document.getElementById('TexteModif');
+	element.style.color='green'
+}
+function ModifImage(Image){
+	var img=document.getElementById(Image);
+	if(img.src.match("Images/feu_rouge.jpg")){
+		img.src="Images/feu_vert.jpg";	
+	}
+	else{
+		img.src="Images/feu_rouge.jpg";
+	}
+}
+function AfficherMasquer(){
+	var img=document.getElementById("Image");
+	if(img.style.display==="none"){
+		img.style.display="block";	
+	}
+	else{
+		img.style.display="none";
+	}
+}
+function setWhite(){
+    document.getElementById("paragraphe").style.color="white";
+  }
+function setInitial(){
+    document.getElementById("paragraphe").style.color="DodgerBlue";
+  }
+function changeColor(){
+  const button=document.getElementById("myButton");
+  var randomColor="#"+Math.floor(Math.random()*16777215).toString(16);
+  document.body.style.backgroundColor=randomColor;
+  if(button.style.color==="red") {
+    button.style.color="black";
+  } else {
+    button.style.color="red";
+  }
+}
